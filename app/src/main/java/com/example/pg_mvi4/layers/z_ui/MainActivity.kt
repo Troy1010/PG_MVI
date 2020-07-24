a8f37e31d739cbb05784622b4b7ea3b635705ede
Troy Swanson <2troy2020@gmail.com>
2020-07-23 22:01:04 -0500

    intentStream

--- app/src/main/java/com/example/pg_mvi4/layers/z_ui/MainActivity.kt
+++ app/src/main/java/com/example/pg_mvi4/layers/z_ui/MainActivity.kt
@@ -3,21 +3,32 @@ package com.example.pg_mvi4.layers.z_ui
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.pg_mvi4.R
+import com.example.pg_mvi4.models.Intent
+import com.example.tmcommonkotlin.logz
+import io.reactivex.rxjava3.subjects.PublishSubject
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+    val intentStream = PublishSubject.create<Intent>()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         setupClickListeners()
+        setupObservers()
+    }
+
+    private fun setupObservers() {
+        intentStream.subscribe {
+            logz("intentStream`it:$it")
+        }
     }
 
     private fun setupClickListeners() {
         btn_1.setOnClickListener {
-            textView1.text = 5.toString()
+            intentStream.onNext(Intent.BumpXCount1)
         }
         btn_2.setOnClickListener {
-            textView2.text = 7.toString()
+            intentStream.onNext(Intent.BumpXCount2)
         }
     }
 }
---
+++ app/src/main/java/com/example/pg_mvi4/models/Intent.kt
@@ -0,0 +1,6 @@
+package com.example.pg_mvi4.models
+
+sealed class Intent {
+    object BumpXCount1:Intent()
+    object BumpXCount2:Intent()
+}
