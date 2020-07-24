f7c714e6560b41a340d04b8d642ee99af4c064c5
Troy Swanson <2troy2020@gmail.com>
2020-07-23 21:57:44 -0500

    basic view

--- app/src/main/java/com/example/pg_mvi4/layers/z_ui/MainActivity.kt
+++ app/src/main/java/com/example/pg_mvi4/layers/z_ui/MainActivity.kt
@@ -3,10 +3,21 @@ package com.example.pg_mvi4.layers.z_ui
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.pg_mvi4.R
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        setupClickListeners()
+    }
+
+    private fun setupClickListeners() {
+        btn_1.setOnClickListener {
+            textView1.text = 5.toString()
+        }
+        btn_2.setOnClickListener {
+            textView2.text = 7.toString()
+        }
     }
 }
